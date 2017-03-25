367aac43d216dc67bcd8678586341d4dd30e06a0
Matt <carlotta.matt@gmail.com>
2017-03-25 13:45:10 -0700

    Adjust source for beta testing

--- ssdtGen.sh
+++ ssdtGen.sh
@@ -4,6 +4,18 @@
 #
 # Version 0.0.1beta - Copyright (c) 2017 by M.F.C.
 #
+# Introduction:
+#     - ssdtGen is an automated bash script that attempts to build and 
+#        compile SSDTs for X99 systems running Mac OS!
+#     - Simply run the commands in the readme to download and execute the
+#        ssdtGen.sh file from your Desktop.
+#
+#
+# Bugs:
+#     - Bug reports can be filed at: https://github.com/mattcarlotta/ssdtGen/issues
+#        Please provide clear steps to reproduce the bug and the output of the
+#        script. Thank you!
+#
 
 #===============================================================================##
 ## GLOBAL VARIABLES #
